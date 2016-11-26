46cabb81cd44c156691aa767b7751defe9fe4ea1
jamesbyrne89 <mail@jamestbyrne.com>
2016-11-26 12:51:27 +0000

    Reworked JS and added reset button

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,115 +1,163 @@
-$(document).ready(function(){
+ 
 
-
-var count = parseInt($('#session-num').html());
-var breakCount = parseInt($('#break-num').html());
-var counter = parseInt($('#counter').html());
-
-$('#reset-button').hide();
-$("#remaining-time").hide();
-
-// Session time buttons
-
-$('#session-less').click(function(){
-	if (count > 5)
-count -= 5;
-$('#session-num').html(count);
-$('#counter').html(count+":00");
-
-});
-
-$('#session-more').click(function(){
-	if (count > 5)
-count += 5;
-$('#session-num').html(count);
-$('#counter').html(count+":00");
-});
-
-// Break time buttons
-
-$('#break-less').click(function(){
-	if (breakCount > 5);
-breakCount -= 5;
-$('#break-num').html(breakCount);
-});
-
-$('#break-more').click(function(){
-	if (breakCount > 5);
-breakCount += 5;
-$('#break-num').html(breakCount);
-});
-
-// Start countdown
-
-$('#start-button').click(function(){
-var counter = setInterval(timer, 1000);
-count*=60;
-function timer(){
-
-// Show time remaining
-$('#reset-button').show();
-$("#remaining-time").show();
-// Hide variables
-$("#start-button").hide();
-$('.buttons-wrapper').hide();
-	count -= 1;
-if (count===0){
-
-	// Play sound
-	var myAudio = new Audio("Temple_Bell.mp3");
-	myAudio.play();
-	clearInterval(counter);
-	var startBreak = setInterval (breakTimer, 1000);
-}
-if (count%60>=10){
-	$('#counter').html(Math.floor(count/60)+":"+count%60);
-	}
-else{
-	$('#counter').html(Math.floor(count/60)+":"+"0"+count%60);
-}
-
-
-// Start break countdown
-breakCount*=60;
-function breakTimer(){
-	
-	$("#remaining-time").show();
-	$("#remaining-time").html('Remaining break time: ');
-	$("#break-num").show();
-	breakCount -= 1;
-
-	// Play sound
-	var myAudio = new Audio("Temple_Bell.mp3");
-	myAudio.play();
-
-	 $("#break-num, #remaining-time").hide();
-		if(breakCount===0){
-clearInterval(startBreak);
- $("#break-num, #remaining-time").hide();
-	}
-	 if (breakCount%60>=10){
-	$('#counter').html(Math.floor(breakCount/60)+":"+breakCount%60);
-	}
-else{
-	$('#counter').html(Math.floor(breakCount/60)+":"+"0"+breakCount%60);
-}
-}
-}
-
-});
-
-// Reset button
-
-$('#reset-button').click(function(){
-
-$('#counter').html(Math.floor(count/60)+":"+count%60);
-clearInterval(timer);
-clearInterval(breakTimer);
-$('#break-num').html(breakCount);
- $(".buttons-wrapper, #break-num, #remaining-time").show();
-
-})
+    $('document').ready(function() {
 
 
 
-});
+    var i = 0;
+    var j = 0;
+    var sessionLength = 25;
+    var breakTimevar = 5;
+    var timer;
+    var seconds;
+    var onlyseconds;
+    var minutes;
+    var currentSession = sessionLength;
+      /* Buttons */
+
+      $('#session-more').click(function() {
+        sessionLength++;
+        console.log(sessionLength);
+        $('#session-num').html(sessionLength);
+        $('#time').html(sessionLength+":00");
+      });
+      $('#session-less').click(function() {
+        if (sessionLength > 1) {
+          sessionLength--;
+          $('#time').html(sessionLength+":00");
+        }
+        $('#session-num').html(sessionLength);
+      });
+      $('#break-more').click(function() {
+        breakTimevar++;
+        $('#break-num').html(breakTimevar);
+      });
+      $('#break-less').click(function() {
+        if (breakTimevar > 1) {
+          breakTimevar--;
+        }
+        $('#break-num').html(breakTimevar);
+      });
+
+      $('#start-button').click(function() {
+
+        switch (i) {
+
+          case 0:
+         
+            currentSession = sessionLength;
+            seconds = currentSession * 60;
+            minutes = Math.round(currentSession);
+            onlyseconds = 0;
+            i = 1;
+            if (j === 0) {
+
+              started();
+              
+            } else
+              breakTimeStart();
+            break;
+
+          case 1:
+            i = 0;
+            stopped();
+            break;
+        }
+      });
+
+      function started() {
+         $('#start-button').text('Pause');
+        j = 0;
+        $('#remaining-time').html('Time remaining:');
+
+        seconds -= 1;
+        if (onlyseconds <= 0.999) {
+          minutes--;
+        }
+        onlyseconds = seconds % 60;
+        onlyseconds = onlyseconds.toFixed(0);
+        if (onlyseconds < 10) {
+          onlyseconds = '0' + onlyseconds;
+        }
+        $('#time').html(minutes + ':' + onlyseconds);
+        if (minutes < 0) {
+          $('#time').html('0:00');
+        }
+        if (minutes >= 0) {
+          timer = setTimeout(function() {
+            started();
+          }, 1000);
+        } else {
+          playSound();
+          breakTimeStart();
+        }
+      };
+
+      function breakTimeStart() {
+        j = 1;
+        currentSession = breakTimevar;
+        seconds = currentSession * 60;
+        minutes = currentSession;
+        onlyseconds = 0;
+        
+        breaktime();
+      };
+
+      function breaktime() {
+        $('#remaining-time').html('Break time');
+        seconds -= 1;
+        if (onlyseconds <= 0.999) {
+          minutes--;
+        }
+        onlyseconds = seconds % 60;
+        onlyseconds = onlyseconds.toFixed(0);
+        if (onlyseconds < 10) {
+          onlyseconds = '0' + onlyseconds;
+        }
+        $('#time').html(minutes + ':' + onlyseconds);
+        if (minutes < 0) {
+          $('#time').html('0:00');
+        }
+        if (minutes >= 0) {
+          timer = setTimeout(function() {
+            breaktime();
+          }, 1000);
+        } else {
+          playSound();
+        }
+      }
+      if (minutes < 0) {
+        $('#time').html('0:00');
+      }
+
+      function stopped() {
+        sessionLength = seconds / 60;
+        clearTimeout(timer);
+        $('#start-button').text('Start');
+      }
+
+      /* Play sound */
+
+      function playSound() {
+        var gong = new Audio('Temple_Bell.mp3');
+        gong.play();
+      }
+
+      /* Reset button */
+
+      $('#reset-button').click(function() {
+        clearTimeout(timer);
+        i = 0;
+        j = 0;
+        breakTimevar = 6;
+        timer = 0;
+        seconds = 0;
+        onlyseconds = 0;
+        minutes = 0;
+        currentSession = Math.round(sessionLength);
+        $('#start-button').text('Start');
+        $('#time').html(currentSession+":00");
+
+      });
+
+    });
